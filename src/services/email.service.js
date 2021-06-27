ba00aa149f4ad308cd0295f580c83652e08fc929
LordRonz <aarontanhar2000@gmail.com>
2021-06-27 21:48:12 +0700

    dev server

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -8,7 +8,9 @@ if (config.env !== 'test') {
   transport
     .verify()
     .then(() => logger.info('Connected to email server'))
-    .catch(() => logger.warn('Unable to connect to email server. Make sure you have configured the SMTP options in .env'));
+    .catch((e) =>
+      logger.warn(`Unable to connect to email server. Make sure you have configured the SMTP options in .env ${e}`)
+    );
 }
 
 /**
