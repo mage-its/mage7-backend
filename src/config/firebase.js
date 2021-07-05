583b81cbccc0ad7bfb4ffd96bc37581edd5e45cc
LordRonz <aarontanhar2000@gmail.com>
2021-07-05 19:48:32 +0700

    fixes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../../serviceAccountKey.json');
+// const serviceAccount = require('../../serviceAccountKey.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
 module.exports = admin;
