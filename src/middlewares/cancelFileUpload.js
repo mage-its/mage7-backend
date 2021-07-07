36332ebd47e5204ff565ec5b47588bad03dec9b5
LordRonz <aarontanhar2000@gmail.com>
2021-07-07 14:32:33 +0700

    removes console.log

--- src/middlewares/cancelFileUpload.js
+++ src/middlewares/cancelFileUpload.js
@@ -2,7 +2,6 @@ const { removeFile } = require('../utils/removeFile');
 
 const cancelFileUpload = () => async (err, req, res, next) => {
   if (req.files) {
-    console.log(req.files);
     await removeFile(req.files);
   }
   next(err);
