7287f1cd7fe845e57417a7431390ca6b5724b826
LordRonz <aarontanhar2000@gmail.com>
2022-01-02 21:30:59 +0700

    docs: gamedev swagger api docs (partially)

--- src/routes/v1/gameDev.route.js
+++ src/routes/v1/gameDev.route.js
@@ -68,3 +68,135 @@ router.post('/inc-tahap/:gameDevId', auth('manageUsers'), validate(gameDevValida
 router.post('/dec-tahap/:gameDevId', auth('manageUsers'), validate(gameDevValidation.decTahap), gameDevController.decTahap);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Authentication
+ */
+
+/**
+ * @swagger
+ * /appdev/daftar-gamedev:
+ *   post:
+ *     summary: Daftar Game Development
+ *     tags: [GameDev]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - kategori
+ *               - namaTim
+ *               - namaKetua
+ *               - waKetua
+ *               - lineKetua
+ *               - hpKetua
+ *               - asalInstansi
+ *               - alamatInstansi
+ *               - asalInfo
+ *               - asalKota
+ *               - identitasKetua
+ *               - buktiUploadTwibbon
+ *               - buktiFollowMage
+ *               - buktiRepostStory
+ *             properties:
+ *               kategori:
+ *                 type: string
+ *                 description: Siswa atau Mahasiswa
+ *               namaTim:
+ *                 type: string
+ *               namaKetua:
+ *                 type: string
+ *               waKetua:
+ *                 type: string
+ *                 description: valid indonesian phone number
+ *               lineKetua:
+ *                 type: string
+ *                 description: id line
+ *               hpKetua:
+ *                 type: string
+ *                 description: valid indonesian phone number
+ *               namaAnggota1:
+ *                 type: string
+ *               namaAnggota2:
+ *                 type: string
+ *               namaPembimbing:
+ *                 type: string
+ *               waPembimbing:
+ *                 type: string
+ *                 description: valid indonesian phone number
+ *               hpPembimbing:
+ *                 type: string
+ *                 description: valid indonesian phone number
+ *               asalInstansi:
+ *                 type: string
+ *               alamatInstansi:
+ *                 type: string
+ *               asalInfo:
+ *                 type: string
+ *               asalKota:
+ *                 type: string
+ *               identitasKetua:
+ *                 type: file
+ *               identitasAnggota1:
+ *                 type: file
+ *               identitasAnggota2:
+ *                 type: file
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 gameDev:
+ *                   $ref: '#/components/schemas/Game'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateEmail'
+ */
+
+/**
+ * @swagger
+ * /gamedev/:
+ *   get:
+ *     summary: Get the gameDevs
+ *     description: GET gameDevs.
+ *     tags: [GameDev]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/GameDev'
+ *                 page:
+ *                   type: integer
+ *                   example: 1
+ *                 limit:
+ *                   type: integer
+ *                   example: 10
+ *                 totalPages:
+ *                   type: integer
+ *                   example: 1
+ *                 totalResults:
+ *                   type: integer
+ *                   example: 1
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
